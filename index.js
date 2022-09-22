aea9aa0d67f0a4905173ff63c4b0ce519f6b38fe
Kirtan Desai <kirtands44@gmail.com>
2022-09-22 11:56:48 -0400

    Set up express

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
+const express = require('express')
 const {fetchNews, sendNews} = require('./app');
 
-(async () => {
-    sendNews(await fetchNews())
-})()
+const app = express()
 
+app.use(express.static("public"))
 
+// app.get("/", (req, res) => {
+//     console.log("Here")
+//     res.send("HEHEHE")
+// })
+
+// (async () => {
+//     sendNews(await fetchNews())
+// })()
+
+app.listen(3000)
