2627f874661cc25676d3de3c48f38be73ede9526
Kirtan Desai <kirtands44@gmail.com>
2022-09-23 17:28:36 -0400

    error handling and sending sample mail on sign up

--- apiHandlers.js
+++ apiHandlers.js
@@ -1,12 +1,15 @@
 const fetch = require('node-fetch')
 require('dotenv').config()
 
-const news_api = "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey="
+const fetchNews = async (category = 'general') => {
+    const news_api = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${process.env.NEWS_API_KEY}`
 
-const fetchNews = async () => {
-    const res = await fetch(news_api + process.env.NEWS_API_KEY)
+    const res = await fetch(news_api)
+
+    if (!res.ok) throw new Error("News API is not working")
+    
     const json = await res.json()
-    news = json.articles.slice(0, 5)
+    news = json.articles.slice(0, Math.min(json.articles.length, 5))
 
     let news_obj = {}
     for (i in news) {
@@ -19,7 +22,7 @@ const fetchNews = async () => {
     return news_obj
 }
 
-const sendNews = async (news_obj) => {
+const sendNews = async (news_obj, email = process.env.TEST_EMAIL) => {
     const options = {
         method: 'POST',
         headers: {
@@ -30,18 +33,17 @@ const sendNews = async (news_obj) => {
         body: JSON.stringify({
             "message": {
                 "to": {
-                    "email": process.env.TEST_EMAIL,
+                    "email": email,
                     "data": news_obj
                 },
                 "template": "BNN4HNWFKCMVVPK8NNW6NAQ9F7EF"
             }
         })
-    };
-
-    console.log(options)
+    }
 
     const res = await fetch('https://api.courier.com/send', options)
 
+    if (!res.ok) throw new Error("Courier API is not working")
 }
 
 module.exports = { fetchNews, sendNews }
