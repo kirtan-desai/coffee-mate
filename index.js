184fd91ec8fce4fc91c2da0ddb250b5e8e62fac9
Kirtan Desai <kirtands44@gmail.com>
2022-09-23 14:20:14 -0400

    validation for categories

--- index.js
+++ index.js
@@ -15,21 +15,16 @@ app.use(express.urlencoded({ extended: true }))
 app.post('/',
     body('email').trim().rtrim().notEmpty().withMessage('Email is empty').isEmail().withMessage('Invalid email'),
     body('name').trim().rtrim().notEmpty().withMessage('Name is empty').isLength({ max: 100 }),
-    body('checkbox-categories').isArray({ min: 1, max: 6 }),
-    body('checkbox-categories.*').isIn(['health', 'general', 'science']),
+    body('checkbox-categories').isArray({ min: 1, max: 5 }).withMessage('Categories should be an array with length between 1 and 5 inclusive'),
+    body('checkbox-categories.*').isIn(['sports', 'technology', 'science', 'business', 'health']).withMessage('Invalid categories'),
     async (req, res) => {
-
         const errors = validationResult(req);
-        if (!errors.isEmpty()) {
-            return res.status(400).json({ errors: errors.array() });
-        }
-
-
+        if (!errors.isEmpty()) return res.status(400).json({ errors: errors.array() });
         try {
             //validate data before posting
-
-            const doc = db.collection('users').doc(req.body.email);
-            await doc.set(req.body)
+            console.log(req.body)
+            // const doc = db.collection('users').doc(req.body.email);
+            // await doc.set(req.body)
             res.send('Record saved successfully')
         } catch (error) {
             res.status(400).send(error.message)
@@ -42,7 +37,6 @@ TODO:
 3. set up chron job to send emails
 4. update data when user already exists
 5. handle duplicates of checkboxes
-
 */
 
 
